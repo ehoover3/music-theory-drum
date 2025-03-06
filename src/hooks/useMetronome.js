ef252d07cfd7da27ac40429b3421fc8fb1bffd9b
Eric Hoover <ehoover3@yahoo.com>
2025-03-06 07:11:05 -0500

    refactor function syntax to const

--- src/hooks/useMetronome.js
+++ src/hooks/useMetronome.js
@@ -1,11 +1,6 @@
 import { useEffect, useRef } from "react";
 
-const resetCycle = (setCurrentCycle, setStartTime) => {
-  setCurrentCycle((prev) => prev + 1);
-  setStartTime(Date.now());
-};
-
-function playSound(metronomeCountSounds, metronomeCountSoundsAlternate, count, currentCycle) {
+const playSound = (metronomeCountSounds, metronomeCountSoundsAlternate, count, currentCycle) => {
   const soundSet = currentCycle % 2 === 0 ? metronomeCountSounds : metronomeCountSoundsAlternate;
   const sound = soundSet.current[count - 1];
   if (sound) {
@@ -13,7 +8,12 @@ function playSound(metronomeCountSounds, metronomeCountSoundsAlternate, count, c
   } else {
     console.warn("Sound not found for count:", count);
   }
-}
+};
+
+const resetCycle = (setCurrentCycle, setStartTime) => {
+  setCurrentCycle((prev) => prev + 1);
+  setStartTime(Date.now());
+};
 
 export const useMetronome = (count, setCount, isPlaying, setCurrentCycle, setStartTime, beatPerMillisecond, currentCycle) => {
   const metronomeCountSounds = useRef([new Audio("/one.mp3"), new Audio("/two.mp3"), new Audio("/three.mp3"), new Audio("/four.mp3")]);
