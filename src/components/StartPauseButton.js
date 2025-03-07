e8f2d5db315ad0e3befa085e57cafa8817588a4a
Eric Hoover <ehoover3@yahoo.com>
2025-03-06 20:08:45 -0500

    update play pause buttons

--- src/components/StartPauseButton.js
+++ src/components/StartPauseButton.js
@@ -1,17 +1,18 @@
 // src/components/StartPauseButton.js
 import React from "react";
 import styles from "./StartPauseButton.module.css";
+import { FaPause, FaPlay } from "react-icons/fa";
 
 const StartPauseButton = ({ isPlaying, startGame, pauseGame }) => {
   return (
     <>
       {!isPlaying ? (
         <button className={styles.startButton} onClick={startGame}>
-          ▶️ Start
+          <FaPlay />
         </button>
       ) : (
         <button className={styles.pauseButton} onClick={pauseGame}>
-          ⏸️ Pause
+          <FaPause />
         </button>
       )}
     </>
