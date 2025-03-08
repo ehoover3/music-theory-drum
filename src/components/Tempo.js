9a947e1ca7be91a5e7cfd20308b072112092110f
Eric Hoover <ehoover3@yahoo.com>
2025-03-08 07:29:14 -0500

    update tempo css

--- src/components/Tempo.js
+++ src/components/Tempo.js
@@ -1,8 +1,16 @@
 import React from "react";
 import "./Tempo.css";
+import { RiArrowDropDownLine } from "react-icons/ri";
 
 function Tempo() {
-  return <div>Tempo</div>;
+  return (
+    <div className='Tempo'>
+      <span>_200 bpm</span>
+      <span>
+        <RiArrowDropDownLine size={40} />
+      </span>
+    </div>
+  );
 }
 
 export default Tempo;
