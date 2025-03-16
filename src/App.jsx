5ef71a3a859734f0ab52d77421c70d89e769112a
Eric Hoover <ehoover3@yahoo.com>
2025-03-15 22:42:20 -0400

    remove unused files

--- src/App.jsx
+++ src/App.jsx
@@ -134,32 +134,3 @@ const App = () => {
 };
 
 export default App;
-
-// import React, { useState, useEffect } from "react";
-// import "./App.css";
-// import Debug from "./components/Debug.jsx";
-// import MusicStaff from "./components/MusicStaff.jsx";
-
-// import notes from "./database/rhythms.json";
-// import ProgressBar from "./components/ProgressBar.jsx";
-
-// function App() {
-//   const [bpm, setBpm] = useState(60);
-//   const [count, setCount] = useState(0);
-//   const [isPlaying, setIsPlaying] = useState(false);
-//   const [progressBar, setProgressBar] = useState(0);
-//   const [taps, setTaps] = useState([]);
-//   const zeroBasedNoteBeats = notes.map((note) => ({ ...note, beat: note.beat - 1 }));
-//   useMetronome(bpm, count, isPlaying, setCount);
-
-//   return (
-//     <div className='grid-container'>
-
-//       <ProgressBar progressBar={progressBar} />
-//       <MusicStaff count={count} notes={zeroBasedNoteBeats} />
-//       <Debug count={count} isPlaying={isPlaying} progressBar={progressBar} taps={taps} />
-//     </div>
-//   );
-// }
-
-// export default App;
--- src/components/UserDots.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./UserDots.css";
-
-function UserDots() {
-  return <div className='tap-dots'>test</div>;
-}
-
-export default UserDots;
